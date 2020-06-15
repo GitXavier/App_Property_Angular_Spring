89e9067d5b7c48cd9e3f7d8d5e430effdb231d69
Xavier <x.crochet@gmail.com>
2020-06-15 10:01:52 +0200

    feat: (property) initialisation component property and property-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyComponent } from './pages/property/property.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { PropertyListComponent } from './pages/property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyComponent,
+    PropertiesComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule,
