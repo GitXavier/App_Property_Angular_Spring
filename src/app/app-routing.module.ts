0d3636b556c85f58049abfb070e0abd6eb92987c
Xavier <x.crochet@gmail.com>
2020-06-15 13:56:29 +0200

    feat: (property) Add component properties/edit and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PropertyComponent } from './pages/property/property.component';
+import { PropertyEditComponent } from './pages/property/property-edit/property-edit.component';
 
 
 const routes: Routes = [
   {
     path: 'properties',
     component: PropertyComponent
+  },
+  {
+    path: 'properties/edit',
+    component: PropertyEditComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyComponent } from './pages/property/property.component';
 import { PropertyListComponent } from './pages/property/property-list/property-list.component';
+import { PropertyEditComponent } from './pages/property/property-edit/property-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyComponent,
-    PropertyListComponent
+    PropertyListComponent,
+    PropertyEditComponent
   ],
   imports: [
     BrowserModule,
